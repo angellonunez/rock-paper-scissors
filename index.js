4cce8f6398c505fe4158df55fdc71161f2691f65
Angello Nunez <angellonzgz@gmail.com>
2022-03-04 01:01:40 -0500

    New functions in JS to show WIN, LOSE or TIE and close function to refresh the page

--- index.js
+++ index.js
@@ -1,37 +1,73 @@
+// Set up variables.
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
+let xWin = document.getElementById('x-win');
+let xTie = document.getElementById('x-tie');
+let xLose = document.getElementById('x-lose');
 
+// Logic -> Computer has 3 values, like your selection. Just one value is going to be randomly selected. There are 3 possible solutions: win, tie or lose.
 rock.addEventListener('click', function rockSelected() {
     let computerValues = ['Rock', 'Paper', 'Scissors']
     let computerResult = computerValues[Math.floor(Math.random() * computerValues.length)];
     if (computerResult == 'Rock') {
-        return alert('You selected Rock and the computer selected ' + computerResult + '.\nIT IS A DRAW!');
+        return document.getElementById("tie").style.display = "inline-block";
     } else if (computerResult == 'Paper') {
-        return alert('You selected Rock and the computer selected ' + computerResult + '.\nYOU LOSE!')
+        return document.getElementById("lose").style.display = "inline-block";
     } else {
-        return alert('You selected Rock and the computer selected ' + computerResult + '.\nYOU WIN!');
+        return document.getElementById("win").style.display = "inline-block";
     }
 });
 paper.addEventListener('click', function paperSelected() {
     let computerValues = ['Rock', 'Paper', 'Scissors']
     let computerResult = computerValues[Math.floor(Math.random() * computerValues.length)];
     if (computerResult == 'Rock') {
-        return alert('You selected Paper and the computer selected ' + computerResult + '.\nYOU WIN!');
+        return document.getElementById("win").style.display = "inline-block";
     } else if (computerResult == 'Paper') {
-        return alert('You selected Paper and the computer selected ' + computerResult + '.\nIT IS A DRAW!')
+        return document.getElementById("tie").style.display = "inline-block";
     } else {
-        return alert('You selected Paper and the computer selected ' + computerResult + '.\nYOU LOSE!');
+        return document.getElementById("lose").style.display = "inline-block";
     }
 });
 scissors.addEventListener('click', function playerSelection() {
     let computerValues = ['Rock', 'Paper', 'Scissors']
     let computerResult = computerValues[Math.floor(Math.random() * computerValues.length)];
     if (computerResult == 'Rock') {
-        return alert('You selected Scissors and the computer selected ' + computerResult + '.\nYOU LOSE!');
+        return document.getElementById("lose").style.display = "inline-block";
     } else if (computerResult == 'Paper') {
-        return alert('You selected Scissors and the computer selected ' + computerResult + '.\nYOU WIN!')
+        return document.getElementById("win").style.display = "inline-block";
     } else {
-        return alert('You selected Scissors and the computer selected ' + computerResult + '.\nIT IS A DRAW!');
+        return document.getElementById("tie").style.display = "inline-block";
     }
+});
+
+// X Close function. It is rudimentary, but it works.
+xWin.addEventListener('click', function close() {
+    window.location.reload();
+})
+
+xTie.addEventListener('click', function close() {
+    window.location.reload();
+})
+
+xLose.addEventListener('click', function close() {
+    window.location.reload();
+})
+
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape" && document.getElementById("lose").style.display == "inline-block"){
+		window.location.reload();
+	}
+});
+
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape" && document.getElementById("win").style.display == "inline-block"){
+		window.location.reload();
+	}
+});
+
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape" && document.getElementById("tie").style.display == "inline-block"){
+		window.location.reload();
+	}
 });
