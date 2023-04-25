b4e7315e407e026a6a4d35837f34150f6ae18de2
Angello Nunez <angellonzgz@gmail.com>
2023-04-25 18:05:39 -0400

    Add scores, elements order, change playRound() structure and create new function called game() to play multiple rounds

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
 let options = ['rock', 'paper', 'scissors']
+let computerScore = 0;
+let playerScore = 0;
+
+let playRound = () => {
 
 // getComputerChoice() is a function that allows you to get a random element from the array options. Math.random() is the most important function to get a pseudo-random number between 0 (inclusive) to 1 (exclusive), but Math.floor() is necessary because it rounds down and returns the largest integer smaller than or equal to the given number.
+    let getComputerChoice = () => {
+        return options[Math.floor(Math.random() * options.length)];
+    }
 
-let getComputerChoice = () => {
-    let computerHand = options[(Math.floor(Math.random() * options.length))];
-    return computerHand;
-}
+    let getPlayerChoice = () => {
+        let playerHand = prompt('Select your hand - Rock, paper or scissors?:');
+        return playerHand;
+    }
 
-let playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
+    let playerSelection = getPlayerChoice().toLowerCase();
+    let computerSelection = getComputerChoice();
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You Lose! Paper beats Rock';
+        computerScore++;
+        console.log('You Lose! Paper beats Rock');
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You Lose! Scissors beats Paper'
+        computerScore++;
+        console.log('You Lose! Scissors beats Paper');
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You Lose! Rock beats Scissors'
+        computerScore++;
+        console.log('You Lose! Rock beats Scissors');
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You Won! Paper beats Rock'
+        playerScore++;
+        console.log('You Won! Paper beats Rock');
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You Won! Scissors beats Paper'
+        playerScore++;
+        console.log('You Won! Scissors beats Paper');
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You Won! Rock beats Scissors'
+        playerScore++;
+        console.log('You Won! Rock beats Scissors');
+    } else if (playerSelection === computerSelection) {
+        console.log('Tie');
     } else {
-        return 'Tie';
+        playRound();
     }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+let game = () => {
+    while (computerScore < 5 || playerScore < 5) {
+        playRound();
+        console.log(computerScore);
+        console.log(playerScore);
+        if (computerScore === 5) {
+            return alert("Computer won!");
+        } else if (playerScore === 5) {
+            return alert("Player won!")
+        }
+    }
+}
+
+game();
